ad0cef0f455f5e740085eac223500396d0fadf1b
Oli Finn <oliver.james.finn@gmail.com>
2022-04-22 15:05:20 +0100

    done

--- wordle_solver/settings.py
+++ wordle_solver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +77,11 @@ WSGI_APPLICATION = 'wordle_solver.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'PostgreSQL 14',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': '<db_host>',
+        'PORT': '5433',
     }
 }
 
