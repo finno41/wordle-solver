b9fa3b6f9e3c952c95f84cf937a3b482ced982b8
Oli Finn <oliver.james.finn@gmail.com>
2022-04-24 14:54:01 +0100

    models  created

--- wordle_bot/models.py
+++ wordle_bot/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
+class Score(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    date = models.DateField()
+    score = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+
+
+class Word(models.Model):
+    name = models.CharField(max_length=5)
